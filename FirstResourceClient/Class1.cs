2d8250b5fb14e0318656832c030e66e265fcc273
王宁 <vipning@qq.com>
2021-03-11 18:29:32 +0800

    完成

--- FirstResourceClient/Class1.cs
+++ FirstResourceClient/Class1.cs
@@ -1,7 +1,7 @@
 ﻿using CitizenFX.Core;
+using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using static CitizenFX.Core.Native.API;
+using System.IO;
 
 namespace FirstResourceClient.net
 {
@@ -9,48 +9,17 @@ namespace FirstResourceClient.net
     {
         public Class1()
         {
-            EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
+            EventHandlers["playerSpawned"] += new Action<dynamic>(OnPlayerSpawned);
         }
 
-        private void OnClientResourceStart(string resourceName)
+        private bool IsFirstSpawn = true;
+        private void OnPlayerSpawned(dynamic spawnInfo)
         {
-            if (GetCurrentResourceName() != resourceName) return;
-
-            RegisterCommand("car", new Action<int, List<object>, string>(async (source, args, raw) =>
+            if (IsFirstSpawn)
             {
-                // account for the argument not being passed
-                var model = "adder";
-                if (args.Count > 0)
-                {
-                    model = args[0].ToString();
-                }
-
-                // check if the model actually exists
-                // assumes the directive `using static CitizenFX.Core.Native.API;`
-                var hash = (uint)GetHashKey(model);
-                if (!IsModelInCdimage(hash) || !IsModelAVehicle(hash))
-                {
-                    TriggerEvent("chat:addMessage", new
-                    {
-                        color = new[] { 255, 0, 0 },
-                        args = new[] { "[CarSpawner]", $"It might have been a good thing that you tried to spawn a {model}. Who even wants their spawning to actually ^*succeed?" }
-                    });
-                    return;
-                }
-
-                // create the vehicle
-                var vehicle = await World.CreateVehicle(model, Game.PlayerPed.Position, Game.PlayerPed.Heading);
-
-                // set the player ped into the vehicle and driver seat
-                Game.PlayerPed.SetIntoVehicle(vehicle, VehicleSeat.Driver);
-
-                // tell the player
-                TriggerEvent("chat:addMessage", new
-                {
-                    color = new[] { 255, 0, 0 },
-                    args = new[] { "[CarSpawner]", $"^*{model}已添加" }
-                });
-            }), false);
+                TriggerServerEvent("sp:serverPlayerSpawned", "useless");
+                IsFirstSpawn = false;
+            }
         }
     }
 }
--- FirstResourceServer/Class1.cs
+++ FirstResourceServer/Class1.cs
@@ -1,14 +1,69 @@
 ﻿using CitizenFX.Core;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using static CitizenFX.Core.Native.API;
 
 namespace FirstResourceServer.net
 {
-    public class Class1
+    public class Class1 : BaseScript
     {
         public Class1()
         {
+            EventHandlers.Add("sp:serverPlayerSpawned", new Action<Player, string>(PlayerSpawned));
+            EventHandlers["playerDropped"] += new Action<Player, string>(OnPlayerDropped);
+        }
+
+        private async void PlayerSpawned([FromSource] Player player, string useless)
+        {
+            await Delay(0);
+            try
+            {
+                if (File.Exists("position.json"))
+                {
+                    var data = File.ReadAllText("position.json");
+                    if (!string.IsNullOrEmpty(data))
+                    {
+                        var positions = JsonConvert.DeserializeObject<Dictionary<string, Vector3>>(data);
+                        var licenseIdentifier = player.Identifiers["license"];
+                        if (positions.TryGetValue(licenseIdentifier, out Vector3 position))
+                        {
+                            player.Character.Position = position;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Debug.WriteLine(ex.StackTrace);
+            }
+        }
+
+        private async void OnPlayerDropped([FromSource] Player player, string reason)
+        {
+            var position = player.Character.Position;
+            var licenseIdentifier = player.Identifiers["license"];
+            Dictionary<string, Vector3> positions = new Dictionary<string, Vector3>();
+            await Delay(0);
+            if (File.Exists("position.json"))
+            {
+                position.Z += 1;
+                var data = File.ReadAllText("position.json");
+                if (!string.IsNullOrEmpty(data))
+                    positions = JsonConvert.DeserializeObject<Dictionary<string, Vector3>>(data);
+
+                if (positions.ContainsKey(licenseIdentifier))
+                    positions[licenseIdentifier] = position;
+                else
+                    positions.Add(licenseIdentifier, position);
+            }
+            else
+            {
+                positions.Add(licenseIdentifier, position);
+            }
+            File.WriteAllText("position.json", JsonConvert.SerializeObject(positions));
         }
     }
 }
