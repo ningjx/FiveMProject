b8972b3427fa6d09504d2a568e9510943762160c
王宁 <vipning@qq.com>
2021-03-12 10:14:00 +0800

    Move "SetPlayerCoords" to client script

--- FirstResourceClient/Class1.cs
+++ FirstResourceClient/Class1.cs
@@ -9,6 +9,12 @@ namespace FirstResourceClient.net
         public Class1()
         {
             EventHandlers["playerSpawned"] += new Action<dynamic>(OnPlayerSpawned);
+            EventHandlers.Add("sp:clientPlayerSpawned", new Action<float, float, float>(PlayerSpawned));
+        }
+
+        private void PlayerSpawned(float x, float y, float z)
+        {
+            SetEntityCoords(GetPlayerPed(-1), x, y, z, false, false, false, false);
         }
 
         private bool IsFirstSpawn = true;
--- FirstResourceServer/Class1.cs
+++ FirstResourceServer/Class1.cs
@@ -18,41 +18,33 @@ namespace FirstResourceServer.net
         private async void PlayerSpawned([FromSource] Player player, string useless)
         {
             await Delay(0);
-            try
+            if (File.Exists("position.json"))
             {
-                if (File.Exists("position.json"))
+                var data = File.ReadAllText("position.json");
+                if (!string.IsNullOrEmpty(data))
                 {
-                    var data = File.ReadAllText("position.json");
-                    if (!string.IsNullOrEmpty(data))
+                    var positions = JsonConvert.DeserializeObject<Dictionary<string, Tuple<float, float, float>>>(data);
+                    var licenseIdentifier = player.Identifiers["license"];
+                    if (positions.TryGetValue(licenseIdentifier, out Tuple<float, float, float> position))
                     {
-                        var positions = JsonConvert.DeserializeObject<Dictionary<string, Vector3>>(data);
-                        var licenseIdentifier = player.Identifiers["license"];
-                        if (positions.TryGetValue(licenseIdentifier, out Vector3 position))
-                        {
-                            player.Character.Position = position;
-                        }
+                        TriggerClientEvent("sp:clientPlayerSpawned", position.Item1, position.Item2, position.Item3);
                     }
                 }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-                Debug.WriteLine(ex.StackTrace);
             }
         }
 
         private async void OnPlayerDropped([FromSource] Player player, string reason)
         {
-            var position = player.Character.Position;
+            var position = new Tuple<float, float, float>(player.Character.Position.X, player.Character.Position.Y, player.Character.Position.Z);
             var licenseIdentifier = player.Identifiers["license"];
-            Dictionary<string, Vector3> positions = new Dictionary<string, Vector3>();
+            var positions = new Dictionary<string, Tuple<float, float, float>>();
             await Delay(0);
             if (File.Exists("position.json"))
             {
-                position.Z += 1;
+                //position.Z += 1;
                 var data = File.ReadAllText("position.json");
                 if (!string.IsNullOrEmpty(data))
-                    positions = JsonConvert.DeserializeObject<Dictionary<string, Vector3>>(data);
+                    positions = JsonConvert.DeserializeObject<Dictionary<string, Tuple<float, float, float>>>(data);
 
                 if (positions.ContainsKey(licenseIdentifier))
                     positions[licenseIdentifier] = position;
